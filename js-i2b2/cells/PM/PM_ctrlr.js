e7d72f9b8867d251acbef508f9c64cfdcb994857
Andrew Post <andrew.post55@gmail.com>
2016-05-26 17:23:13 -0400

    Erase the CAS_ticket cookie in the unexpected response condition

--- js-i2b2/cells/PM/PM_ctrlr.js
+++ js-i2b2/cells/PM/PM_ctrlr.js
@@ -149,6 +149,7 @@ i2b2.PM._processUserConfig = function (data) {
 	    		i2b2.PM.doCASLogin(data.msgParams.sec_domain)
 	    		return true;
 	    	} else {
+	    		eraseCookie("CAS_ticket");
 			console.error("I2b2 web client did not get a user account back. The user may be unauthorized, or the i2b2 server was restarted.");
 			alert("I2b2 web client got an unexpected response from the i2b2 server. Click OK and try reloading the page.");
 			return false;
