859b013b31422978447d89c2e2be6f874b4c8e1a
Andrew Post <arpost@emory.edu>
2016-10-08 10:05:21 -0400

    Comment out the ProjectRequest plugin.

--- js-i2b2/i2b2_loader.js
+++ js-i2b2/i2b2_loader.js
@@ -58,14 +58,31 @@ i2b2.hive.tempCellsList = [
 		   forceConfigMsg: { params: [] },
 		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
 		   forceDir: "cells/plugins/standard"
-
-		}//,
-        // { code: "ProjectRequest",
-        //     forceLoading: true,
-        //     forceConfigMsg: { params: [] },
-	// 	   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
-        //     forceDir: "cells/plugins/standard"
-        // }
+		},
+		{ code:	"WISEsearcher",
+		   forceLoading: true,
+		   forceConfigMsg: { params: [] },
+		   roles: [ "DATA_OBFSC", "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
+		   forceDir: "cells/plugins/community"
+		},
+		{ code:	"CAREcncptDem",
+		   forceLoading: true,
+		   forceConfigMsg: { params: [] },
+		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
+		   forceDir: "cells/plugins/community"
+		},
+		{ code:	"CAREobsTally",
+		   forceLoading: true,
+		   forceConfigMsg: { params: [] },
+		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
+		   forceDir: "cells/plugins/community"
+		},
+		{ code:	"ExportXLS",
+		   forceLoading: true,
+		   forceConfigMsg: { params: [] },
+		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
+		   forceDir: "cells/plugins/community"
+		}		
 		
 	];
 // ================================================================================================== //
