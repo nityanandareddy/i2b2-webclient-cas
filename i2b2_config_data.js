ab96ca40e4b9408a456ff2131deef808f081aa57
Jason Sands <jsands79@gmail.com>
2017-06-21 12:19:29 -0400

    1_7_08a merge conflicts

--- i2b2_config_data.js
+++ i2b2_config_data.js
@@ -1,15 +1,15 @@
 {
 	urlProxy: "index.php",
-	urlFramework: "js-i2b2/",
+    urlFramework: "js-i2b2/",
+    casDomain: "i2b2demo",
 	//-------------------------------------------------------------------------------------------
 	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
 	lstDomains: [
 		{ domain: "i2b2demo",
 		  name: "HarvardDemo",
-		  urlCellPM: "http://services.i2b2.org/i2b2/services/PMService/",
+		  urlCellPM: "http://webservices.i2b2.org/i2b2/services/PMService/",
 		  allowAnalysis: true,
-		  //installer: "/webclient/plugin_installer/",
-		  debug: false
+		  debug: false,
 		  "CAS_server": "https://localhost:8443/cas-server/"
 		}
 	]
